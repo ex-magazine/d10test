10107df50843cc12ff6fb6fb51d4bdfd4957463f
Tommy <asaleotestf@gmail.com>
2024-02-20 16:09:23 +0700

    first

--- web/themes/custom/solar/js/main.js
+++ web/themes/custom/solar/js/main.js
@@ -499,13 +499,42 @@
             });
           });
 
-          $(document).ready(function(){
+          $(document).ready(function(){        
+            /*
+            var mySwiper = new Swiper ('.mySwiper-banner-2', {             
+              direction: 'horizontal',
+              loop: false,            
+              // Navigation arrows
+              navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+              },              
+              on: {
+                transitionStart: function(){                  
+                  var videos = document.querySelectorAll('video');            
+                  Array.prototype.forEach.call(videos, function(video){
+                    video.pause();
+                  });
+                },                
+                transitionEnd: function(){                  
+                  var activeIndex = this.activeIndex;
+                  var activeSlide = document.getElementsByClassName('swiper-slide')[activeIndex];
+                  var activeSlideVideo = activeSlide.getElementsByTagName('video')[0];
+                  activeSlideVideo.play();                
+                },              
+              }
+            })
+            
+            */
+
+            
+            /*
             var swiper = new Swiper(".mySwiper-banner-2", {
               pagination: {
                 el: ".swiper-pagination-b2",
                 clickable: true
               },
-                navigation: {
+              navigation: {
                 nextEl: ".swiper-button-next",
                 prevEl: ".swiper-button-prev",
               },
@@ -515,8 +544,83 @@
                 delay: 5000,
               },
             });
-          });
+            */
 
+
+            // variable 
+            var VIDEO_PLAYING_STATE = {
+              "PLAYING": "PLAYING",
+              "PAUSE": "PAUSE"
+            };
+            var videoPlayStatus = VIDEO_PLAYING_STATE.PAUSE;
+            var timeout = null;
+            var waiting = 3000;
+            var swiper = new Swiper(
+              '.mySwiper-banner-2', {
+              navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+              },
+            });
+
+            // HTML5 vdo object
+            var options = {};
+            var player = videojs('my-player', options);
+            player.on('ended', function() {  
+              swiper.slideNext();  
+            })
+            // swiper object
+            swiper.on('slideChangeTransitionEnd', function () {
+              
+              var index = swiper.activeIndex
+              var currentSlide =   $(swiper.slides[index])
+              var currentSlideType = currentSlide.data('slide-type')
+              
+              // incase user click next before video ended
+              if (videoPlayStatus === VIDEO_PLAYING_STATE.PLAYING) {
+                player.pause()
+              }
+              
+              clearTimeout(timeout)
+              
+              switch (currentSlideType) {
+                case 'img':
+                  runNext()
+                  break;
+                case 'vdo':
+                  player.currentTime(0)
+                  player.play()
+                  videoPlayStatus = VIDEO_PLAYING_STATE.PLAYING
+                  break;
+                default:
+                  throw new Error('invalid slide type');
+              }
+            })
+
+            // global function
+            function prev() {
+              swiper.slidePrev();
+            }
+
+            function next() {
+              swiper.slideNext();  
+            }
+
+            function runNext() {  
+              timeout = setTimeout(function () {
+                next()
+              }, waiting)
+            }
+            unmuteButton.addEventListener('click', function() {
+              video.muted = false;
+            
+            });
+            runNext();
+
+
+
+          });
+          // variable 
           $(document).ready(function(){
             var swiper = new Swiper(".mySwiper-testimonials-solari", {
               pagination: {
