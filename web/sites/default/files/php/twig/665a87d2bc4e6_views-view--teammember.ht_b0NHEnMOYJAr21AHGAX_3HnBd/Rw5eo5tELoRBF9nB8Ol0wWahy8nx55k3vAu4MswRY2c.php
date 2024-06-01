9859c95518cdb8b477540f7a1b90d40286afa79b
root <root@debian.debian>
2024-05-31 22:56:56 -0400

    first

---
+++ web/sites/default/files/php/twig/665a87d2bc4e6_views-view--teammember.ht_b0NHEnMOYJAr21AHGAX_3HnBd/Rw5eo5tELoRBF9nB8Ol0wWahy8nx55k3vAu4MswRY2c.php
@@ -0,0 +1,237 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/solar/templates/views/views-view--teammember.html.twig */
+class __TwigTemplate_e8361ee8985ac7b42a9882004e869dd9 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "
+";
+        // line 35
+        $context["classes"] = [0 => "view", 1 => "rts-team-area", 2 => ("view-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(        // line 38
+($context["id"] ?? null), 38, $this->source))), 3 => ("view-id-" . $this->sandbox->ensureToStringAllowed(        // line 39
+($context["id"] ?? null), 39, $this->source)), 4 => ("view-display-id-" . $this->sandbox->ensureToStringAllowed(        // line 40
+($context["display_id"] ?? null), 40, $this->source)), 5 => ((        // line 41
+($context["dom_id"] ?? null)) ? (("js-view-dom-id-" . $this->sandbox->ensureToStringAllowed(($context["dom_id"] ?? null), 41, $this->source))) : (""))];
+        // line 44
+        echo "
+<div";
+        // line 45
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method", false, false, true, 45), 45, $this->source), "html", null, true);
+        echo "> 
+  ";
+        // line 46
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_prefix"] ?? null), 46, $this->source), "html", null, true);
+        echo "
+  ";
+        // line 47
+        if (($context["title"] ?? null)) {
+            // line 48
+            echo "    ";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title"] ?? null), 48, $this->source), "html", null, true);
+            echo "
+  ";
+        }
+        // line 50
+        echo "  ";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_suffix"] ?? null), 50, $this->source), "html", null, true);
+        echo "
+  ";
+        // line 51
+        if (($context["header"] ?? null)) {
+            echo "  
+      ";
+            // line 52
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["header"] ?? null), 52, $this->source), "html", null, true);
+            echo "
+  ";
+        }
+        // line 54
+        echo "  ";
+        if (($context["exposed"] ?? null)) {
+            // line 55
+            echo "    <div class=\"view-filters\">
+      ";
+            // line 56
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["exposed"] ?? null), 56, $this->source), "html", null, true);
+            echo "
+    </div>
+  ";
+        }
+        // line 59
+        echo "  ";
+        if (($context["attachment_before"] ?? null)) {
+            // line 60
+            echo "    <div class=\"attachment attachment-before\">
+      ";
+            // line 61
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attachment_before"] ?? null), 61, $this->source), "html", null, true);
+            echo "
+    </div>
+  ";
+        }
+        // line 64
+        echo "
+  ";
+        // line 65
+        if (($context["rows"] ?? null)) {
+            // line 66
+            echo "      <div class=\"row g-4\">     
+            ";
+            // line 67
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["rows"] ?? null), 67, $this->source), "html", null, true);
+            echo "        
+      </div>
+  ";
+        } elseif (        // line 69
+($context["empty"] ?? null)) {
+            // line 70
+            echo "    <div class=\"view-empty\">
+      ";
+            // line 71
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["empty"] ?? null), 71, $this->source), "html", null, true);
+            echo "
+    </div>
+  ";
+        }
+        // line 74
+        echo "
+  ";
+        // line 75
+        if (($context["pager"] ?? null)) {
+            // line 76
+            echo "    ";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["pager"] ?? null), 76, $this->source), "html", null, true);
+            echo "
+  ";
+        }
+        // line 78
+        echo "  ";
+        if (($context["attachment_after"] ?? null)) {
+            // line 79
+            echo "    <div class=\"attachment attachment-after\">
+      ";
+            // line 80
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attachment_after"] ?? null), 80, $this->source), "html", null, true);
+            echo "
+    </div>
+  ";
+        }
+        // line 83
+        echo "  ";
+        if (($context["more"] ?? null)) {
+            // line 84
+            echo "    ";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["more"] ?? null), 84, $this->source), "html", null, true);
+            echo "
+  ";
+        }
+        // line 86
+        echo "  ";
+        if (($context["footer"] ?? null)) {
+            // line 87
+            echo "    <div class=\"view-footer\">
+      ";
+            // line 88
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["footer"] ?? null), 88, $this->source), "html", null, true);
+            echo "
+    </div>
+  ";
+        }
+        // line 91
+        echo "  ";
+        if (($context["feed_icons"] ?? null)) {
+            // line 92
+            echo "    <div class=\"feed-icons\">
+      ";
+            // line 93
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["feed_icons"] ?? null), 93, $this->source), "html", null, true);
+            echo "
+    </div>
+  ";
+        }
+        // line 95
+        echo "  
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/solar/templates/views/views-view--teammember.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  185 => 95,  179 => 93,  176 => 92,  173 => 91,  167 => 88,  164 => 87,  161 => 86,  155 => 84,  152 => 83,  146 => 80,  143 => 79,  140 => 78,  134 => 76,  132 => 75,  129 => 74,  123 => 71,  120 => 70,  118 => 69,  113 => 67,  110 => 66,  108 => 65,  105 => 64,  99 => 61,  96 => 60,  93 => 59,  87 => 56,  84 => 55,  81 => 54,  76 => 52,  72 => 51,  67 => 50,  61 => 48,  59 => 47,  55 => 46,  51 => 45,  48 => 44,  46 => 41,  45 => 40,  44 => 39,  43 => 38,  42 => 35,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/custom/solar/templates/views/views-view--teammember.html.twig", "/var/www/html/adn.id.vn/d10test/web/themes/custom/solar/templates/views/views-view--teammember.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 35, "if" => 47);
+        static $filters = array("clean_class" => 38, "escape" => 45);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if'],
+                ['clean_class', 'escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
